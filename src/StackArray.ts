7833fae4a003272afc0e555d850abb91dab3555c
Fredson Chaves <fredsonchaves07@gmail.com>
2021-04-21 18:40:20 -0300

    feat: implementation stack

---
+++ src/StackArray.ts
@@ -0,0 +1,34 @@
+class StackArray<T>{
+    
+    private items: T[] 
+
+    constructor(){
+        this.items = []
+    }
+
+    push(element: T){
+
+    }
+
+    pop(){
+
+    }
+
+    peek(){
+
+    }
+
+    isEmpty(){
+
+    }
+
+    clear(){
+
+    }
+
+    size(){
+        
+    }
+}
+
+export { StackArray }
